6e36e0a2d37c2862d27a5365040219d9fab1b900
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-05 17:27:49 +0330

    public static path

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import { pathJoin } from "./util/filesystem";
 
 const app = express();
 
-// register template engin
+// register template engine
 app.set("view engine", "ejs");
+// define static folders
+app.use(express.static(pathJoin("public")));
 
 // routes
 app.use(routes);
