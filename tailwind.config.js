e3a8eae2c75dc27d07f536a467b029b05e833c47
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-05 20:33:46 +0330

    add flowbite for write tailwind components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: ["./views/**/*.ejs", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
