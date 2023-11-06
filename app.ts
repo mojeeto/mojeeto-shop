c83975b98e3bd0ecff2d5396b845479cb18d8019
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-06 15:24:33 +0330

    install mongoose and set env file configs

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import routes from "./routes";
 import { pathJoin } from "./util/filesystem";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -9,4 +12,14 @@ app.use(express.static(pathJoin("public")));
 
 app.use(routes);
 
-app.listen(3000);
+const mongoUrl = process.env.MONGO_URL_CONNECT;
+const mongoDb = process.env.MONGO_URL_DB;
+mongoose
+  .connect(mongoUrl! + mongoDb!)
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log("Error: Bad url for connecting DB!\n", err);
+    process.exit();
+  });
---
+++ types/env.d.ts
@@ -0,0 +1,4 @@
+export interface ProccessEnv {
+  MONGO_URL_CONNECT: string | undefined;
+  MONGO_URL_DB: string | undefined;
+}
