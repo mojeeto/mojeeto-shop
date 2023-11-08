3f25e646f95c126f63a8a003a6be1195c593eef1
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-08 22:51:48 +0330

    add multer and more validation in add-product path

--- middleware/parserMiddleware.ts
+++ middleware/parserMiddleware.ts
@@ -1,8 +1,27 @@
+import multer from "multer";
 import bodyParser from "body-parser";
 import { Router } from "express";
 
 const parserMiddleware = Router();
+const multerStorage = multer.diskStorage({
+  filename(req, file, callback) {
+    callback(null, new Date().toISOString() + file.originalname);
+  },
+  destination(req, file, callback) {
+    callback(null, "images");
+  },
+});
 
 parserMiddleware.use(bodyParser.urlencoded({ extended: false }));
+parserMiddleware.use(
+  multer({
+    storage: multerStorage,
+    fileFilter(req, file, callback) {
+      if (["image/jpeg", "image/png"].find((item) => file.mimetype === item))
+        return callback(null, true);
+      callback(null, false);
+    },
+  }).single("image")
+);
 
 export default parserMiddleware;
--- routes/admin.ts
+++ routes/admin.ts
@@ -3,14 +3,34 @@ import {
   getAddProduct,
   getManageProducts,
   getManageUsers,
+  postAddProduct,
 } from "../controllers/adminController";
 import { isAuthenticate } from "../middleware/authMiddleware";
 import { isAdmin } from "../middleware/adminMiddleware";
+import { body } from "express-validator";
 
 const adminRouter = Router();
 
 adminRouter.get("/manage-products", isAuthenticate, isAdmin, getManageProducts);
 adminRouter.get("/manage-users", isAuthenticate, isAdmin, getManageUsers);
 adminRouter.get("/add-product", isAuthenticate, isAdmin, getAddProduct);
+
+adminRouter.post(
+  "/add-product",
+  isAuthenticate,
+  isAdmin,
+  [
+    body("title")
+      .isAlphanumeric()
+      .withMessage("Name must alphanumeric.")
+      .isLength({ min: 2, max: 32 })
+      .withMessage("Name length between 2 & 32 character."),
+    body(
+      "description",
+      "The description length most be between 25 and 255 character."
+    ).isLength({ min: 25, max: 255 }),
+  ],
+  postAddProduct
+);
 
 export default adminRouter;
