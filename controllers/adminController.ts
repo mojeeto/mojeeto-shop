4ea1f24c22bc2086701b87c62dd47a34caf3e143
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-10 12:06:19 +0330

    validation fields and error add-product

--- controllers/adminController.ts
+++ controllers/adminController.ts
@@ -1,4 +1,10 @@
+import { validationResult } from "express-validator";
 import { Controller } from "./controller";
+import {
+  getErrorValidationFieldMsg,
+  getErrorValidationFields,
+} from "../util/validation";
+import Product from "../models/Product";
 
 export const getManageProducts: Controller = (req, res, next) => {
   res.render("pages/admin/manage-products");
@@ -10,4 +16,31 @@ export const getManageUsers: Controller = (req, res, next) => {
 
 export const getAddProduct: Controller = (req, res, next) => {
   res.render("pages/admin/add-product");
+};
+
+export const postAddProduct: Controller = (req, res, next) => {
+  const { title, price, quantity, description } = req.body;
+  const validationErrors = validationResult(req);
+  const image = req.file;
+  if (!validationErrors.isEmpty()) {
+    const errorsValidation = getErrorValidationFieldMsg(
+      validationErrors.array()
+    );
+    // TODO:: error fields not working fix that
+    const errorFields = getErrorValidationFields(validationErrors.array());
+    return res.render("pages/admin/add-product", {
+      path: "/manage-products",
+      values: { ...req.body },
+      errors: errorsValidation,
+      errorFields,
+    });
+  }
+  if (!image) {
+    // TODO:: ADD IMAGE
+    return res.render("pages/admin/add-product", {
+      path: "/manage-products",
+      values: { ...req.body },
+    });
+  }
+  res.redirect("/add-product");
 };
