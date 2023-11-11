69b4d77c36e64c621c6a5f1b485a08989a1379f0
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-11 10:36:41 +0330

    add product detail page

--- controllers/shopController.ts
+++ controllers/shopController.ts
@@ -1,4 +1,5 @@
 import Product from "../models/Product";
+import { flashAddMessage } from "../util/flash";
 import { Controller } from "./controller";
 
 export const getShop: Controller = (req, res, next) => {
@@ -7,4 +8,14 @@ export const getShop: Controller = (req, res, next) => {
       res.render("index", { path: "/", products });
     })
     .catch((err) => next(new Error("Error while getHome")));
+};
+
+export const getProduct: Controller = (req, res, next) => {
+  const { productId } = req.params;
+  Product.findOne({ _id: productId })
+    .then((product) => {
+      if (product) return res.render("pages/product", { product: product });
+      next();
+    })
+    .catch((error) => next(error));
 };
--- routes/shop.ts
+++ routes/shop.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getShop } from "../controllers/shopController";
+import { getProduct, getShop } from "../controllers/shopController";
 
 const shopRouter = Router();
 
 // Home Page
 shopRouter.get("/", getShop);
+shopRouter.get("/:productId", getProduct);
 
 export default shopRouter;
